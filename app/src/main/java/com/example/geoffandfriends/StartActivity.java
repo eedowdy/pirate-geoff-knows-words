52e2f9af7e06e68c1428bf6ca78a9bfaf952ffbf
eedowdy <54999769+eedowdy@users.noreply.github.com>
2019-12-02 15:12:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/geoffandfriends/StartActivity.java
@@ -0,0 +1,25 @@
+package com.example.geoffandfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button startGame = findViewById(R.id.newGameButton);
+        startGame.setOnClickListener(unused -> start());
+    }
+
+    public void start() {
+        final Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
