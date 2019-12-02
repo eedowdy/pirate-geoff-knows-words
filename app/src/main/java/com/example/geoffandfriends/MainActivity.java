6cb194707990059344352b902b3377bfaba2e835
eedowdy <54999769+eedowdy@users.noreply.github.com>
2019-12-02 16:12:46 -0600

    Pirate Geoff is in the building

--- app/src/main/java/com/example/geoffandfriends/MainActivity.java
+++ app/src/main/java/com/example/geoffandfriends/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.geoffandfriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        word = "geoffrey";
 
+        char[] letters = word.toCharArray();
+
+        TextView displayWord = findViewById(R.id.displayWord);
+        StringBuilder sb = new StringBuilder();
+        for (char letter : letters) {
+            sb.append("_ ");
+        }
+        displayWord.setText(sb.toString());
+
+        
     }
 }
